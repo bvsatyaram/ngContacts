78d182356b9e3e31bf0fad374c12c0759ab5fcfd
BV Satyaram <bvsatyaram@gmail.com>
2015-09-26 07:57:39 +0530

    Link table row to show page of contact

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,5 +1,5 @@
 angular.module('ContactsApp')
-  .controller('ListController', ['Contact', function(Contact){
+  .controller('ListController', ['Contact', '$location', function(Contact, $location){
     list = this;
     list.contacts = Contact.query();
     list.fields = ['firstName', 'lastName'];
@@ -11,4 +11,8 @@ angular.module('ContactsApp')
 
     list.sortField = 'firstName';
     list.sortOrder = false;
+
+    list.show = function(id) {
+      $location.url('/contact/' + id)
+    };
   }]);
